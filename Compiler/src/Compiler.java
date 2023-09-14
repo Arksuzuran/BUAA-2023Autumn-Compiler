943aa9d78d2b7aa7c554921a7fdbebe572394420
czx <2471280802@qq.com>
2023-09-14 13:19:58 +0800

    feat: java init

---
+++ Compiler/src/Compiler.java
@@ -0,0 +1,11 @@
+/**
+ * @Description entrance
+ * @Author  HIKARI
+ * @Date  2023/9/14
+ **/
+public class Compiler {
+
+    public static void main(String[] args) {
+        System.out.println("hell, word!");
+    }
+}
---
+++ Compiler/src/frontend/Lexer.java
@@ -0,0 +1,11 @@
+package frontend;
+
+/**
+ * @Description TODO
+ * @Author H1KARI
+ * @Date 2023/9/14
+ **/
+public class Lexer {
+
+
+}
